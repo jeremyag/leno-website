bc73afae8fc57adacd219270a4f7d8d0ebb1ba71
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-12-16 20:23:52 +0800

    Added video modal.

--- js/script.js
+++ js/script.js
@@ -6,14 +6,36 @@ document.addEventListener("DOMContentLoaded", function() {
 		mobileMenu.classList.toggle("active");
 	});
 
-	window.addEventListener("scroll", function() {
-		const navbar = document.querySelector(".navbar");
-		
-		if(window.scrollY > 0){
-			navbar.classList.add("navbar--scroll");
-		}else{
-			navbar.classList.remove("navbar--scroll");
-		}
+	const modal = document.getElementById("videoModal");
+	const videoButton = document.querySelector(".preview__video-button");
+	const closeButton = document.querySelector(".modal__close-button");
+	const videoPlayer = document.getElementById("videoPlayer");
+
+	videoButton.addEventListener("click", function() {
+		modal.style.display = "block";
+
+		videoPlayer.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?si=uLjZL1-KmTL83lXm";
 	});
 
+	closeButton.addEventListener("click", function() {
+		modal.style.display = "none";
+		videoPlayer.src = "";
+	});
+
+	window.addEventListener("click", function(event) {
+		if(event.target == modal){
+			modal.style.display = "none";
+			videoPlayer.src = "";
+		}
+	})
+});
+
+window.addEventListener("scroll", function() {
+	const navbar = document.querySelector(".navbar");
+	
+	if(window.scrollY > 0){
+		navbar.classList.add("navbar--scroll");
+	}else{
+		navbar.classList.remove("navbar--scroll");
+	}
 });
